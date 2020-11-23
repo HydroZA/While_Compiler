c4bbacb737d3fe97e00384edafac02696b9e25a7
HydroZA <jmslegge0@gmail.com>
2020-11-23 17:55:26 +0000

    Initial Interpreter Commit

---
+++ Interpreter/Interpreter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Parser;
+
+namespace Interpreter
+{
+    public class Interpreter
+    {
+        private Dictionary<string, int> Environment;
+
+        public Interpreter()
+        {
+            Environment = new Dictionary<string, int>();
+        }
+
+        public string Interpret(Block code)
+        {
+            EvaluateBlock(code);
+        }
+
+        private void EvaluateBlock(Block code)
+        {
+            // Check if the block is empty
+            if (code.statements.Count == 0)
+            {
+                return;
+            }
+            else
+            {
+                Statement current = code.statements[0];
+                code.statements.RemoveAt(0);
+
+            }
+        }
+
+        private void EvaluateStatement(Statement s)
+        {
+
+        }
+
+    }
+}
---
+++ Interpreter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Lexer;
+using Parser;
+
+namespace Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
